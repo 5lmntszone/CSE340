0c58ce6e3c6e69efe65148dede7b9ff37aa0bf17
Barbara <5elementszone@gmail.com>
2025-07-30 20:18:15 +0200

    mvc gs

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const baseController = require("./controllers/baseController")
 
 /* ***********************
  * Routes
@@ -20,8 +21,25 @@ app.use(expressLayouts)
 app.set("layout", "./layouts/layout")
 
 // Index route
-app.get("/", function(req, res){
-  res.render("index", {title: "Home"})
+app.get("/", baseController.buildHome)
+
+// Error 404
+app.use(async (req, res, next) => {
+  next({ status: 404, message: "Sorry, we appear to have lost that page."});
+});
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message: err.message,
+    nav
+  })
 })
 
 /* ***********************
