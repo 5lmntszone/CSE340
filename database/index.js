304bc16205050aea46530d871047e578b87974ca
Barbara <5elementszone@gmail.com>
2025-07-31 18:40:31 +0200

    fixed ssl errors

--- database/index.js
+++ database/index.js
@@ -1,29 +1,18 @@
 const { Pool } = require("pg")
 require("dotenv").config()
 
-let pool
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, 
+  },
+})
 
-if (process.env.NODE_ENV === "production") {
-  pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false, 
-    },
-  })
-} else {
-  pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-  })
-}
-
-// Added for troubleshooting queries in development
 module.exports = {
   async query(text, params) {
     try {
       const res = await pool.query(text, params)
-      if (process.env.NODE_ENV !== "production") {
-        console.log("executed query", { text })
-      }
+      console.log("executed query", { text })
       return res
     } catch (error) {
       console.error("error in query", { text })
